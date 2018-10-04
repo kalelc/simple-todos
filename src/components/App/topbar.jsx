1a96d0866d98725cb3bc055d66090ffb3b074047
Andres Colonia <andreskal@gmail.com>
2018-10-04 18:12:44 -0300

    update topbar

--- src/components/App/topbar.jsx
+++ src/components/App/topbar.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 
-import { Link } from "react-router-dom";
 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap"
+import { Link } from "react-router-dom";
 export default class Topbar extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <ul className="nav navbar-nav">
-                <li><Link to='/new'> New </Link></li>
-                <li><Link to='/delete'> Delete </Link></li>
-                <li><Link to='/about'> About </Link></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <Navbar>
+        <Nav>
+          <NavItem eventKey={1}>
+            <Link to='/new'> New </Link>
+          </NavItem>
+          <NavItem eventKey={2}>
+            <Link to='/delete'> Delete </Link>
+          </NavItem>
+          <NavItem eventKey={2}>
+            <Link to='/about'> About </Link>
+          </NavItem>
+        </Nav>
+      </Navbar>
     );
   }
 }
