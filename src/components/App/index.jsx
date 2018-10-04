d2db60a77bb7e6d776d3283a8b655b6bac4dfc54
Andres Colonia <andreskal@gmail.com>
2018-10-04 17:10:08 -0300

    use router switch with links

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import './index.css';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Route } from "react-router";
 
 import Topbar from './topbar';
-
 import New from '../App/new';
 import Delete from '../App/delete';
 import About from '../About/index';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 export default class Index extends Component {
   render() {
     return (
-      <div>
-        <Topbar />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Topbar />
+          <Switch>
+            <Route key="new" exact path='/new' render={(props) => { return <New {...this.props} {...props} /> }} />
+            <Route key="delete" exact path='/delete' render={(props) => { return <Delete {...this.props} {...props} /> }} />
+            <Route key="about" exact path='/about' render={(props) => { return <About {...this.props} {...props} /> }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
