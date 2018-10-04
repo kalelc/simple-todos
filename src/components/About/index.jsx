93cb2eafc8ed75d93e6693924d0b7b310936709a
Andres Colonia <andreskal@gmail.com>
2018-10-04 18:13:46 -0300

    example using alert with react-bootstrap

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
 
 export default class Index extends Component {
   render() {
     return (
-      <div className="About">
-        <header className="About-header">
-          <h1 className="About-title">About</h1>
-        </header>
-        <p className="About-intro">
-          This is about
-        </p>
-      </div>
+      <Alert bsStyle="warning">
+        <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+      good.
+      </Alert>
     );
   }
 }
