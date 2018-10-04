d3785429a13fff76e2d8cf66d716a8bfefaac86f
jocodinan <jorge.codina@gmail.com>
2018-10-04 11:55:01 -0300

    colombian fix

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Route } from "react-router";
+
 import './index.css';
 
 import Topbar from './topbar';
-
 import New from '../App/new';
 import Delete from '../App/delete';
 import About from '../About/index';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 export default class Index extends Component {
   render() {
     return (
-      <div>
-        <Topbar />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Topbar />
+          <Switch>
+            <Route key="new" exact path='/new' render={(props) => { return <New {...this.props} {...props} /> }} />
+            <Route key="delete" exact path='/delete' render={(props) => { return <Delete {...this.props} {...props} /> }} />
+            <Route key="about" exact path='/about' render={(props) => { return <About {...this.props} {...props} /> }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/App/topbar.jsx
+++ src/components/App/topbar.jsx
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
 import './index.css';
 
-import New from '../App/new';
-import Delete from '../App/delete';
-import About from '../About/index';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Link } from "react-router-dom";
 
 export default class Topbar extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <nav className="navbar navbar-default">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <ul className="nav navbar-nav">
-                  <li><Link to='/new'> New </Link></li>
-                  <li><Link to='/delete'> Delete </Link></li>
-                  <li><Link to='/about'> About </Link></li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <Route path='/new' component={New} />
-          <Route path='/delete' component={Delete} />
-          <Route path='/about' component={About} />
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <ul className="nav navbar-nav">
+              <li><Link to='/new'> New </Link></li>
+              <li><Link to='/delete'> Delete </Link></li>
+              <li><Link to='/about'> About </Link></li>
+            </ul>
+          </div>
         </div>
-      </Router>
+      </nav>
     );
   }
 }
